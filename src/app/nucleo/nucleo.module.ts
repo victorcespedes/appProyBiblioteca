52d8dce8db661bb1fe70ffb83f3ea74bf0525e2d
Edgar Becerra <edgarbecerra79@gmail.com>
2019-04-23 21:56:04 -0500

    Agregando los temas

---
+++ src/app/nucleo/nucleo.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NucleoRoutingModule } from './nucleo-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NucleoRoutingModule
+  ]
+})
+export class NucleoModule { }
